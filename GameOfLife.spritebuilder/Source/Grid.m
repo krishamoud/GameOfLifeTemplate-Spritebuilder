9a2c88ad6ef044b375e444f6e4a8c74773544839
Kristopher Hamoud <kristopherhamoud@Kristophers-MacBook-Pro.local>
2014-08-09 17:30:19 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -162,7 +162,7 @@ static const int GRID_COLUMNS = 10;
             if(currentCreature.livingNeighbors == 3)
             {
                 currentCreature.isAlive = YES;
-                numAlive++;
+                ++numAlive;
             }
             else if(currentCreature.livingNeighbors <=1 || currentCreature.livingNeighbors >= 4)
             {
